58886f606a055e6f28bc5f0a9bc884a09d786012
thiago.machado <thiago.machado@seati.ma.gov.br>
2023-04-12 16:41:47 -0300

    feat: implementado métodos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'curso-alura';
+  transferencia: any;
+
+  transferir($event: any) {
+    console.log($event);
+    this.transferencia = $event
+  }
 }
