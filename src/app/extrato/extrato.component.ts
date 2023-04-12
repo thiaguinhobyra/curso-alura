1e3b41c81096c2cc579761187f90eb879b5b6e5e
thiago.machado <thiago.machado@seati.ma.gov.br>
2023-04-12 17:59:39 -0300

    feat: tribuindo array de transferencia em extrato

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ExtratoComponent implements OnInit {
 
-  @Input() transferencia: any;
+  @Input() transferencias?: any[];
 
   constructor() {}
 
