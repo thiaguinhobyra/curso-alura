aa6e45436ae57ea5a05372aa01600111af0d227c
thiago.machado <thiago.machado@seati.ma.gov.br>
2023-04-13 14:57:19 -0300

    refactor: atualizando style para scss

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-extrato',
   templateUrl: './extrato.component.html',
-  styleUrls: ['./extrato.component.css']
+  styleUrls: ['./extrato.component.scss']
 })
 export class ExtratoComponent implements OnInit {
 
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -3,7 +3,7 @@ import { Component, EventEmitter, Output } from "@angular/core";
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.css']
+  styleUrls: ['./nova-transferencia.component.scss']
 })
 
 export class NovaTransferenciaComponent {
